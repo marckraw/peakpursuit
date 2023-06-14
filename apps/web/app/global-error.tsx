20b22ce68cc02be06358389511ef30e2c310a965
marckraw <marckraw@icloud.com>
2023-06-14 07:42:03 +0200

    Add global error page component

---
+++ apps/web/app/global-error.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+const GlobalErrorPage = ({
+                                        error,
+                                        reset,
+                                    }: {
+    error: Error
+    reset: () => void
+}) => {
+    return (
+        <html>
+        <body>
+        <h2>Something went wrong!</h2>
+        <button onClick={() => reset()}>Try again</button>
+        </body>
+        </html>
+    )
+}
+
+export default GlobalErrorPage
