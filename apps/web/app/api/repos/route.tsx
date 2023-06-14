30de8f092d95f869918d6b30bf8c2c0dd9da3f34
marckraw <marckraw@icloud.com>
2023-06-14 07:32:05 +0200

    Add assets folder and public folder to the app:

--- apps/web/app/api/repos/route.tsx
+++ apps/web/app/api/repos/route.tsx
@@ -4,3 +4,8 @@ import repos from "./data.json"
 export async function GET(request) {
     return NextResponse.json(repos)
 }
+
+export async function POST(request) {
+    const course = await request.json()
+    return NextResponse.json(repos)
+}
