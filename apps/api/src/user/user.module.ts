462ada092d4d86592dd23c698cbf5b052e3c7db0
marckraw <marckraw@icloud.com>
2023-07-10 07:12:20 +0200

    Generate auth service

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
 import { UserService } from '../user/user.service';
 import { UserController } from '../user/user.controller';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AuthService],
 })
 export class UserModule {}
